4f6642649dabb4db9a0244537c4dfef9584c639c
iamparas <iamparas@users.noreply.github.com>
2016-12-12 23:30:45 -0500

    Clean up config

--- openVulnQuery/openVulnQuery/config.py
+++ openVulnQuery/openVulnQuery/config.py
@@ -2,5 +2,5 @@
 CLIENT_ID = ""
 CLIENT_SECRET = ""
 
-REQUEST_TOKEN_URL = "https://cloudsso.cisco.com/as/token.oauth2?grant_type=client_credentials"
+REQUEST_TOKEN_URL = "https://cloudsso.cisco.com/as/token.oauth2"
 API_URL = "https://api.cisco.com/security/advisories"
