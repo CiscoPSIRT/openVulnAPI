05e474fff1af424d149ce9fcf40021bf200a772d
santosomar <santosomar@gmail.com>
2018-10-14 09:42:27 -0400

    Update openVulnQuery/setup.py

--- openVulnQuery/setup.py
+++ openVulnQuery/setup.py
@@ -1,11 +1,11 @@
 from setuptools import setup, find_packages
 
 setup(name='OpenVulnQuery',
-      version='1.26',
+      version='1.3',
       description='A python-based module(s) to query the Cisco PSIRT openVuln API.',
       url='https://github.com/CiscoPSIRT/openVulnAPI/tree/master/openVulnQuery',
-      author='Bradley Korabik, Parash Ghimire, Omar Santos',
-      author_email='bkorabik@cisco.com, pghimire@cisco.com, os@cisco.com',
+      author=' Omar Santos',
+      author_email='os@cisco.com',
       license='The MIT License (MIT)',
       packages=find_packages(exclude=["tests"]),
       entry_points={
